51a0a572230983527abfd03ab2fd8b4d79f5411e
Priyanshu Singh Chouhan <112051581+Priyanshu5088@users.noreply.github.com>
2024-09-07 21:07:06 +0530

    Add files via upload

---
+++ 01_Basics/operatorstestii.java
@@ -0,0 +1,11 @@
+public class operatorstestii {
+    public static void main(String args[]){
+        int x = 9 , y = 12;
+        int a =2 , b = 4 , c = 6;
+        int exp =4/3 * (x+34) + 9 * (a+b*c) + (3 + y * (2 + a))/(a+b*y);
+        // 43 + 234 + 1/96;
+        System.out.println(exp);
+
+    }
+    
+}
