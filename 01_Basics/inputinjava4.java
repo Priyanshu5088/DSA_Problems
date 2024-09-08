9772e1523b30d6016fd8f3d95dd3d1926a0159a3
Priyanshu Singh Chouhan <112051581+Priyanshu5088@users.noreply.github.com>
2024-09-08 07:58:15 +0530

    Add files via upload

---
+++ 01_Basics/inputinjava4.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class inputinjava4 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        // TAKING INPUT A SINGLE WORD  
+           String input = sc.next();//NEXT=it is used to capture a single word
+           System.out.println(input);
+
+        // TAKING INPUT A WHOLE SENTENCE 
+        //    String name = sc.nextLine();/*nextline= it is used to capture
+                                         // a full sentence*/ 
+        //    System.out.println(name);
+        }
+    }
+
+    
+
