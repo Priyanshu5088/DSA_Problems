7c2c5860e7214515d7e9848d2a872a45f56bdacc
Priyanshu Singh Chouhan <112051581+Priyanshu5088@users.noreply.github.com>
2024-09-08 07:58:50 +0530

    Add files via upload

---
+++ 01_Basics/Typeconversion5.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Typeconversion5 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        float number = sc.nextInt();
+        System.out.println(number);
+        
+
+      
+       //long num = sc.nextFloat();  /*Output is error because we cannot assign long in
+       //                               float but we can assign float value in long*/ 
+                                   
+      // System.out.println(num);
+       
+       /* ORDER OF TYPE CONVERSION :-
+       byte-> short -> int -> float -> long -> double */ 
+
+    }
+    
+}
