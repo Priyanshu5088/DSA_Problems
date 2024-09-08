cd8cb83db42fe82538d3a432946a370799571895
Priyanshu Singh Chouhan <112051581+Priyanshu5088@users.noreply.github.com>
2024-09-08 08:04:37 +0530

    Add files via upload

---
+++ Operators_in_Java/operatorstest.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class operatorstest {
+    public static void main(String args[]){
+        int x,y,z;
+        x=y=z=2;
+        x+= y;
+        y-= z;
+        z/= (x+y);//Here answer is 0.5, but output comes out to be 0
+        System.out.println(x + "\n" + y + "\n" + z);
+ 
+    }       
+}
