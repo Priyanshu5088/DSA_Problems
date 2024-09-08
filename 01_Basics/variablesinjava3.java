b9d6ccf506f1b4dd0388338a935ce3a81b79cf1f
Priyanshu Singh Chouhan <112051581+Priyanshu5088@users.noreply.github.com>
2024-09-08 08:02:49 +0530

    Add files via upload

---
+++ 01_Basics/variablesinjava3.java
@@ -0,0 +1,23 @@
+public class variablesinjava3 {
+    public static void main(String args[]){
+        int a=10;
+        int b=5;
+        System.out.println(a);
+        System.out.println(b);
+        String name="TONY STARK";
+        System.out.println(name);
+
+        //If we want to change the value of variable 
+        a=50;
+        System.out.println(a);
+        name="STEVE ROGERS";
+        System.out.println(name);
+
+        /*If we want to assign values to 
+        different variables
+        */
+        a=b;
+        System.out.println(a);
+    }
+    
+}
