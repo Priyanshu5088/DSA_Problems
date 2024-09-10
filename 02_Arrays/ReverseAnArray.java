a4efc1f043dd6e19f7f07e8a146520ecda84d699
Priyanshu Singh Chouhan <112051581+Priyanshu5088@users.noreply.github.com>
2024-09-10 21:06:36 +0530

    Add files via upload

---
+++ 02_Arrays/ReverseAnArray.java
@@ -0,0 +1,26 @@
+public class ReverseAnArray {
+
+    public static void Reverse(int arr[]){
+       int first = 0;
+       int last = arr.length-1;
+       
+       while(first<last){
+          int temp = arr[last];
+          arr[last] = arr[first];
+          arr[first] = temp;
+
+          first++;
+          last--;
+       }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {2,4,6,8,10,0};
+        Reverse(arr);
+        for(int i=0;i<=arr.length-1;i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    
+}
