2b4a0172c6d2213fc0cb578c322502315dc9ea2f
Priyanshu Singh Chouhan <112051581+Priyanshu5088@users.noreply.github.com>
2024-09-16 09:21:32 +0530

    Add files via upload

---
+++ 04_Bits_Manipulation/updateIthbit.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class updateIthbit {
+    public static int clearIthBit(int n, int i){
+        int bitMask = ~(1<<i);
+        return n & bitMask;
+    }
+
+    public static int updateIthBit(int n, int i, int newBit){
+        n = clearIthBit(n, i);
+        int bitMask = newBit<<i;
+        return n | bitMask;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(updateIthBit(10, 2, 1));
+    }
+
+    
+}
