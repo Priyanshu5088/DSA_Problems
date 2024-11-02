acc8644dc6f3301b11e741832996625ab0b0a32b
Priyanshu Singh Chouhan <112051581+Priyanshu5088@users.noreply.github.com>
2024-11-02 10:22:43 +0530

    Add files via upload

---
+++ 18_TRIES/Insertion.java
@@ -0,0 +1,35 @@
+public class Insertion {
+    static class Node{
+        Node children[] = new Node[26];
+        boolean eow = false;
+
+        Node(){
+            for(int i=0; i<26; i++){
+                children[i] = null;
+            }
+        }
+    }
+
+    public static Node root = new Node();
+
+    public static void insert(String word){
+        Node curr = root;
+        for(int level=0; level<word.length(); level++){ // Traversing each level
+            int idx = word.charAt(level) - 'a'; // getting the idx of curr alphabet
+            if(curr.children[idx] == null){ // Never exist 
+                curr.children[idx] = new Node();
+            }
+            curr = curr.children[idx]; // update the curr
+        }
+
+        curr.eow = true; // store true at the end of node
+    } 
+
+    public static void main(String[] args) {
+        String word[] = {"the", "a" , "there" , "their" , "any" , "thee"}; 
+        for(int i=0; i<word.length; i++){
+        insert(word[i]);
+        }
+    }
+    
+}
